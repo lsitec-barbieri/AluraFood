4df82f20fb76fe02f8699bc99daeb3c3a42fe663
lsitec-Barbieri <matheus.barbieri@lsitec.org.br>
2022-03-31 13:48:19 -0300

    Commit inicial

---
+++ app/src/main/java/br/com/alura/alurafood/ui/activity/FormularioCadastroActivity.java
@@ -0,0 +1,16 @@
+package br.com.alura.alurafood.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.alura.alurafood.R;
+
+public class FormularioCadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_cadastro);
+    }
+}
